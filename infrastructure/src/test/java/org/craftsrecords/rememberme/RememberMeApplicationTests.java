8eeb7eceb2259be63bb00ee85b2b8276a1537112
Juliette de Rancourt <derancourt.juliette@gmail.com>
2019-03-16 18:36:44 +0100

    Create infrastructure module

---
+++ infrastructure/src/test/java/org/craftsrecords/rememberme/RememberMeApplicationTests.java
@@ -0,0 +1,16 @@
+package org.craftsrecords.rememberme;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RememberMeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
