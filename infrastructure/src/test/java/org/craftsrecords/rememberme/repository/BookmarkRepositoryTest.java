bb4106fc00734e71f2c6b466bb987796f4072b06
Julien Topçu <julien.topcu@gmail.com>
2019-04-02 18:57:06 +0200

    Simplify BookmarkRepositoryTest

--- infrastructure/src/test/java/org/craftsrecords/rememberme/repository/BookmarkRepositoryTest.java
+++ infrastructure/src/test/java/org/craftsrecords/rememberme/repository/BookmarkRepositoryTest.java
@@ -1,11 +1,9 @@
 package org.craftsrecords.rememberme.repository;
 
-import org.craftsrecords.rememberme.bookmark.AlreadyBookmarkedException;
 import org.craftsrecords.rememberme.bookmark.Bookmark;
+import org.craftsrecords.rememberme.bookmark.Bookmarks;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -21,42 +19,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(SpringRunner.class)
 public class BookmarkRepositoryTest {
 
-    private static final String URL = "http://www.test.com";
-
-    @Rule
-    public final ExpectedException exception = ExpectedException.none();
-
     @Autowired
     private JpaBookmarkRepository jpaBookmarkRepository;
 
-    private BookmarkRepository bookmarkRepository;
+    private Bookmarks bookmarks;
     private Bookmark bookmark;
 
     @Before
     public void set_up() {
-        bookmarkRepository = new BookmarkRepository(jpaBookmarkRepository);
-        bookmark = Bookmark.create(URL, "test", singleton("tag"));
+        bookmarks = new BookmarkRepository(jpaBookmarkRepository);
+        bookmark = Bookmark.create("http://www.test.com", "test", singleton("tag"));
     }
 
     @Test
     public void should_save_bookmark() {
-        Bookmark saved = bookmarkRepository.save(bookmark);
+        Bookmark saved = bookmarks.save(bookmark);
         assertThat(saved).isEqualTo(bookmark);
-    }
-
-    @Test
-    public void should_not_save_an_already_existent_bookmark() {
-        jpaBookmarkRepository.save(BookmarkEntity.from(bookmark));
-
-        exception.expect(AlreadyBookmarkedException.class);
-        bookmarkRepository.save(bookmark);
     }
 
     @Test
     public void should_retrieve_bookmark_by_url() {
         jpaBookmarkRepository.save(BookmarkEntity.from(bookmark));
 
-        Optional<Bookmark> retrieved = bookmarkRepository.getBy(URL);
+        Optional<Bookmark> retrieved = bookmarks.getBy(bookmark.getUrl());
         assertThat(retrieved).hasValue(bookmark);
     }
 
@@ -64,7 +49,7 @@ public class BookmarkRepositoryTest {
     public void should_retrieve_all_bookmarks() {
         jpaBookmarkRepository.save(BookmarkEntity.from(bookmark));
 
-        Collection<Bookmark> retrieved = bookmarkRepository.getAll();
+        Collection<Bookmark> retrieved = bookmarks.getAll();
         assertThat(retrieved).contains(bookmark);
     }
 
